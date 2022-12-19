1bf47d2f2b2371e8e5d3719610780e4e50274494
carbonto <davidcarbonell32@outlook.com>
2022-12-19 18:40:01 +0100

    No muestra el estado en interfaz

--- waiter_bot/src/interfaz.py
+++ waiter_bot/src/interfaz.py
@@ -27,6 +27,17 @@ from kivy.lang import Builder
 import webbrowser
 
 
+def process_table(data):
+    global mesa
+    mesa = data.data
+    rospy.loginfo("Mesa: " + str(mesa))
+    Interfaz().screen.ids.estado.text = "Yendo a mesa " + str(mesa)
+def process_estado(data):
+    global estado
+    estado = data.status
+def proccess_load_food(data):
+    global load_food
+    load_food = data.data
 class Interfaz(MDApp):
 
     def __init__(self, **kwargs):
@@ -57,7 +68,6 @@ class Interfaz(MDApp):
         rospy.loginfo("Going to table 1")
         msg = 1
         pub_mesa.publish(msg)
-        self.screen.ids.estado.text = "Yendo a mesa 1"
     def table2(self,*args):
         rospy.loginfo("Going to table 2")
         msg = 2
@@ -85,19 +95,36 @@ class Interfaz(MDApp):
         url = 'http://localhost:8080/stream?topic=/camera/rgb/image_raw&type=ros_compressed'
         webbrowser.open(url)
     #     #App().get_running_app().stop()
-
+    def estado(self,*args):
+        global mesa, estado,load_food
+        if estado == 1 and load_food == 1:
+            self.screen.ids.estado.text = "Yendo a mesa " + str(mesa)
+        elif estado == 3:
+            self.screen.ids.estado.text = "Llegado a mesa"
+            if load_food == 0:
+                self.screen.ids.estado.text = "Bebidas retiradas"
+        elif estado == 4:
+            self.screen.ids.estado.text = "Cancelado"
+        elif estado == 1 and mesa == 0:
+            self.screen.ids.estado.text = "Yendo a home"
 
     #status 1 es yendo mesa y estatus 3 es que ha llegado a la mesa
     #En cada funcion de mesa se publica el numero de mesa a la que se va
     #En cancelar tambien publica que se ha cancelado el movimiento 
     #Con funcion aparte se dice si ha llegado o no 
 #main
+
+
+
 if __name__ == '__main__':
 
     pub_bumper = rospy.Publisher('/bumpsi', UInt8, queue_size=1)
+    sub_bumper = rospy.Subscriber('/bumpsi', UInt8,proccess_load_food)
+    sub_mesa = rospy.Subscriber('/mesa', UInt8,process_table)
     pub_mesa = rospy.Publisher('/mesa', UInt8, queue_size=1)
     pub_cancel = rospy.Publisher('/move_base/cancel',GoalID,queue_size=1)
     pub_cancel2 = actionlib.SimpleActionClient('move_base',MoveBaseAction)
+    sub_estado = rospy.Subscriber('/move_base/status',GoalStatus,process_estado)
     rospy.init_node('Interfaz',anonymous=True)
     # url = 'http://google.com'
     # webbrowser.open(url)
